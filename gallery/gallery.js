3093caf391f938e56028799a14f568717d8a3dc7
starlight0711 <126453408+starlight0711@users.noreply.github.com>
2023-10-12 14:16:53 -0400

    Update gallery.js

--- gallery/gallery.js
+++ gallery/gallery.js
@@ -24,9 +24,9 @@ const folders = [
   const queryString = window.location.search;
 
 
-  //const urlParams = new URLSearchParams(queryString);
-  //const album = urlParams.get('album');
-const album =2;
+  const urlParams = new URLSearchParams(queryString);
+  const album = urlParams.get('album');
+//const album =2;
   var selectedAlbum = 3;
   if(album!=null){
     selectedAlbum= album;
